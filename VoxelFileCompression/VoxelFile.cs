f053ec60fd61aa7e827985e6db4e65c9626547ab
dphsw <dhudson@dphsw.co.uk>
2022-09-05 15:27:28 +0100

    Open MagicaVoxel File

---
+++ VoxelFileCompression/VoxelFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace VoxelFileCompression {
+
+  public class VoxelFile {
+
+    public bool CompressedWhenLoaded { get; private set; }
+    public int VersionIndicator { get; private set; }
+
+    public static VoxelFile Load(Stream stream) {
+
+      if (stream == null) return null;
+
+      VoxelFile voxelFile = new VoxelFile();
+
+      using (BinaryReader br = new BinaryReader(stream)) {
+
+        voxelFile.ReadFirstFourChars(br);
+        voxelFile.ReadVersionIndicator(br);
+
+
+
+      }
+
+      return voxelFile;
+
+    }
+
+    void ReadFirstFourChars(BinaryReader br) {
+      string fourChars = new string(br.ReadChars(4));
+      if (fourChars == "VOX ") CompressedWhenLoaded = false;
+      else if (fourChars == "VOZ ") CompressedWhenLoaded = false;
+      else throw new Exception("File is not a MagicaVoxel file");
+    }
+
+    void ReadVersionIndicator(BinaryReader br) {
+      VersionIndicator = br.ReadInt32();
+    }
+
+  }
+
+}
---
+++ VoxelFileCompressionTests/VoxelFileTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Xunit;
+using VoxelFileCompression;
+
+namespace VoxelFileCompressionTests {
+  public class VoxelFileTests {
+
+    const string TestFilename = "..\\..\\..\\realistic_terrain.vox";
+
+    [Fact]
+    public void TestFileExists() {
+      Assert.NotEqual("", TestFilename);
+      Assert.NotEqual("", File.ReadAllText(TestFilename));
+    }
+
+    [Fact]
+    public void TestFileIsMagicaVoxelFile() {
+      using (FileStream fs = new FileStream(TestFilename, FileMode.Open)) {
+        VoxelFile voxelFile = VoxelFile.Load(fs);
+        Assert.False(voxelFile.CompressedWhenLoaded);
+        Assert.True(voxelFile.VersionIndicator > 0);
+      }
+    }
+
+  }
+}
